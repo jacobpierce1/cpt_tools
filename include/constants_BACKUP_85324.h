7695d69041bc6ace58184fee4a6b2cd0f313c8f3
Jacob <jacobpierce@uchicago.edu>
2018-06-27 10:29:30 -0500

    about to work on windows

--- include/constants_BACKUP_85324.h
+++ include/constants_BACKUP_85324.h
@@ -2,8 +2,8 @@
 #define CONSTANTS_H
 
 
-#define USE_GUI 1
-#define USE_TDC 0
+#define USE_GUI 0
+#define USE_TDC 1
 #define USE_CARIBU 0
 
 #ifndef _WIN32
@@ -16,11 +16,9 @@
 #define TDC_MAX_COUNTS  100000
 
 
+<<<<<<< HEAD
 #define HISTO_DIMX 32
 #define HISTO_DIMY 32
-
-
-
-
-
+=======
 #endif
+>>>>>>> 6801aa1c346579b517267c3dd937bb5d75cc0705
---
+++ include/constants_BASE_85324.h
@@ -0,0 +1,5 @@
+
+#define TDC_HIT_BUFFER_SIZE  1000
+#define TDC_MAX_COUNTS  100000
+
+
